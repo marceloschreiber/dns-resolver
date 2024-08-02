9620b44f90622ba39d3e7964366de225f9aa7700
Marcelo Fernandes <marcelo.schreiber@gmail.com>
2024-08-01 21:22:22 -0300

    Create query header function

--- src/dns_resolver/core.clj
+++ src/dns_resolver/core.clj
@@ -1,18 +1,39 @@
-(ns dns-resolver.core)
+(ns dns-resolver.core
+  (:import [java.nio ByteBuffer]
+           [java.util BitSet]))
+
+(let [bit-set (BitSet. 8)]
+  (.set bit-set 7)
+  (.toByteArray bit-set))
 
 (def type-values
   "A map of TYPES to their values and meanings.
-   See https://www.rfc-editor.org/rfc/rfc1035#section-3.2.2 for a complete list."
+   For a complete list see [ref:type-values]"
   [{:type :A, :value 1, :meaning "a host address"}
    {:type :NS, :value 2, :meaning "an authoritative name server"}
    {:type :CNAME, :value 5, :meaning "the canonical name for an alias"}])
 
 (def class-values
   "A map of CLASS values to their values and meanings.
-   See https://www.rfc-editor.org/rfc/rfc1035#section-3.2.4 for a complete list."
+   For a complete list see [ref:class-values]."
   [{:class :IN, :value 1, :meaning "the Internet"}])
 
-(defn- generate-random-query-id
-  "Generate a random 16-bit ID for a DNS query."
+(defn generate-random-query-id
+  "Generate a random 16-bit ID for a DNS query.
+   For more info see [ref:header-id]."
   []
   (rand-int (Math/pow 2 16)))
+
+(defn generate-query-header
+  "Generate a query header for a DNS query.
+   For more info about the format see [ref:header-section-format]."
+  [{:keys [id recursion-desired?]}]
+  (let [buffer (ByteBuffer/allocate 12)
+        flags (if recursion-desired? 0x0100 0x0000)]
+    (.putShort buffer id) ;;ID
+    (.putShort buffer flags) ;; FLAGS
+    (.putShort buffer 1) ;; QDCOUNT
+    (.putShort buffer 0) ;; ANCOUNT
+    (.putShort buffer 0) ;; NSCOUNT
+    (.putShort buffer 0) ;; ARCOUNT
+    (.array buffer)))
