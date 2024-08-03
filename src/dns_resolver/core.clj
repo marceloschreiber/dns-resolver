7993ef9a25ecf31354fa3f8fe0264e75990bf761
Marcelo Fernandes <marcelo.schreiber@gmail.com>
2024-08-03 16:36:11 -0300

    Write unsigned function

--- src/dns_resolver/core.clj
+++ src/dns_resolver/core.clj
@@ -1,5 +1,4 @@
-(ns dns-resolver.core
-  (:import [java.nio ByteBuffer]))
+(ns dns-resolver.core)
 
 (def type-values
   "A map of TYPES to their values and meanings.
@@ -13,22 +12,43 @@
    For a complete list see [ref:class-values]."
   [{:class :IN, :value 1, :meaning "the Internet"}])
 
+(defn unsigned-bytes->hex
+  "Convert a sequence of bytes to a hex string."
+  [bytes-seq]
+  (->> bytes-seq
+       (map #(format "%02X" %))
+       (apply str)))
+
 (defn generate-random-query-id
   "Generate a random 16-bit ID for a DNS query.
    For more info see [ref:header-id]."
   []
   (rand-int (Math/pow 2 16)))
 
-(defn generate-query-header
+(defn check-value-within-limits
+  "Check if a value is within the limits of a given unsigned number of bits."
+  [value bits]
+  (when (or (< value 0) (>= value (Math/pow 2 bits)))
+    (throw (ex-info "Value out of bounds" {:value value, :bits bits}))))
+
+(defn write-unsigned-16
+  "Write an unsigned 16-bit integer to a byte array.
+   Array must be at least 2 bytes long and the value must be within the 16-bit limit."
+  [array index value]
+  (check-value-within-limits value 16)
+  (aset array index (bit-shift-right (bit-and value 0xFF00) 8))
+  (aset array (inc index) (bit-and value 0x00FF)))
+
+(defn generate-query-header-section
   "Generate a query header for a DNS query.
    For more info about the format see [ref:header-section-format]."
   [{:keys [id recursion-desired?], :or {recursion-desired? true}}]
-  (let [buffer (ByteBuffer/allocate 12)
+  (let [array (int-array 12)
         flags (if recursion-desired? 0x0100 0x0000)]
-    (.putShort buffer id) ;;ID
-    (.putShort buffer flags) ;; FLAGS
-    (.putShort buffer 1) ;; QDCOUNT
-    (.putShort buffer 0) ;; ANCOUNT
-    (.putShort buffer 0) ;; NSCOUNT
-    (.putShort buffer 0) ;; ARCOUNT
-    (.array buffer)))
+    (write-unsigned-16 array 0 id)
+    (write-unsigned-16 array 2 flags)
+    (write-unsigned-16 array 4 1) ;; QDCOUNT
+    (write-unsigned-16 array 6 0) ;; ANCOUNT
+    (write-unsigned-16 array 8 0) ;; NSCOUNT
+    (write-unsigned-16 array 10 0) ;; ARCOUNT
+    array))
--- test/dns_resolver/core_test.clj
+++ test/dns_resolver/core_test.clj
@@ -2,15 +2,72 @@
   (:require [clojure.test :refer [deftest is testing]]
             [dns-resolver.core :as sut]))
 
-(deftest generate-query-header-test
-  (testing "When recursion-desired? is false"
-    (is (= (vec (byte-array [0 1 0 0 0 1 0 0 0 0 0 0]))
-           (vec (sut/generate-query-header {:id 1 :recursion-desired? false})))))
+(defmacro catch-thrown-info [f]
+  `(try
+     ~f
+     (catch
+      clojure.lang.ExceptionInfo e#
+       {:msg (ex-message e#) :data (ex-data e#)})))
 
-  (testing "When recursion-desired? is true"
-    (is (= (vec (byte-array [0 1 1 0 0 1 0 0 0 0 0 0]))
-           (vec (sut/generate-query-header {:id 1 :recursion-desired? true})))))
+(deftest unsigned-bytes->hex-test
+  (is (= "FF0082" (sut/unsigned-bytes->hex [0xFF 0x00 0x82]))))
 
-  (testing "By default recursion-desired? is true"
-    (is (= (vec (byte-array [0 1 1 0 0 1 0 0 0 0 0 0]))
-           (vec (sut/generate-query-header {:id 1}))))))
+(deftest check-value-within-limits-test
+  (testing "Don't throw when value is within limits"
+    (is (nil? (sut/check-value-within-limits 0 8)))
+    (is (nil? (sut/check-value-within-limits 255 8)))
+    (is (nil? (sut/check-value-within-limits 65535 16))))
+
+  (testing "Throw when value is out of bounds"
+    (testing "Negative value"
+      (is (= {:msg "Value out of bounds", :data {:value -1, :bits 8}}
+             (catch-thrown-info (sut/check-value-within-limits -1 8)))))
+
+    (testing "Over the upper limit"
+      (is (= {:msg "Value out of bounds", :data {:value 256, :bits 8}}
+             (catch-thrown-info (sut/check-value-within-limits 256 8)))))))
+
+(deftest write-unsigned-16-test
+  (testing "Write 16-bit value to a byte array"
+    (testing "Write 0"
+      (let [array (int-array 2)]
+        (sut/write-unsigned-16 array 0 0)
+        (is (= [0 0] (into [] array)))))
+
+    (testing "Write 255"
+      (let [array (int-array 2)]
+        (sut/write-unsigned-16 array 0 255)
+        (is (= [0 255] (into [] array)))))
+
+    (testing "Write 256"
+      (let [array (int-array 2)]
+        (sut/write-unsigned-16 array 0 256)
+        (is (= [1 0] (into [] array)))))
+
+    (testing "Write 65535"
+      (let [array (int-array 2)]
+        (sut/write-unsigned-16 array 0 65535)
+        (is (= [255 255] (into [] array)))))))
+
+(deftest generate-query-header-section-test
+  (testing "When recursion enabled the flags should have the RD bit set"
+    (let [header (sut/generate-query-header-section {:id 255 :recursion-desired? true})]
+      (is (= [0 255 ;; ID
+              1 0 ;; Flags
+              0 1 ;; QDCOUNT
+              0 0 ;; ANCOUNT
+              0 0 ;; NSCOUNT
+              0 0 ;; ARCOUNT
+              ]
+             (into [] header)))))
+
+  (testing "When recursion disabled the flags should be 0"
+    (let [header (sut/generate-query-header-section {:id 65535 :recursion-desired? false})]
+      (is (= [255 255 ;; ID
+              0 0 ;; Flags
+              0 1 ;; QDCOUNT
+              0 0 ;; ANCOUNT
+              0 0 ;; NSCOUNT
+              0 0 ;; ARCOUNT
+              ]
+             (into [] header))))))
